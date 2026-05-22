97991600d1a85a4552ec0cadfefb564270d085e7
ayinde-xyz <ayindeabdulrahman16@gmail.com>
2026-05-22 20:53:39 +0100

    refactor: replace session cookie handling with auth API session retrieval

--- proxy.ts
+++ proxy.ts
@@ -1,19 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getSessionCookie } from "better-auth/cookies";
+import { auth } from "./lib/auth";
+import { headers } from "next/headers";
 
 export async function proxy(request: NextRequest) {
-  const sessionCookie = getSessionCookie(request);
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
   const { pathname } = request.nextUrl;
-  console.log("Session Cookie in Middleware:", sessionCookie);
-  // Redirect authenticated users away from login/signup pages
-  if (sessionCookie && ["/auth/login", "/auth/signup"].includes(pathname)) {
-    return NextResponse.redirect(new URL("/chat", request.url));
-  }
 
   // Redirect unauthenticated users trying to access protected routes
-  if (!sessionCookie && pathname.startsWith("/chat")) {
+  if (!session && pathname.startsWith("/chat")) {
     return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+
+  // Redirect authenticated users away from login/signup pages
+  if (session && ["/auth/login", "/auth/signup"].includes(pathname)) {
+    return NextResponse.redirect(new URL("/chat", request.url));
   }
 
   return NextResponse.next();
